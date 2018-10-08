50558735efb52a77fcde3a03144bea5fc2c7b1cb
wallandall <wallandall@gmail.com>
2018-10-08 23:08:29 +0200

    Add modular design, budgetController and UIController

---
+++ app.js
@@ -0,0 +1,17 @@
+//Budget controller
+budgetController = (function() {
+
+})();
+
+
+//UI Controller
+var UIController = (function() {
+
+})();
+
+//APP
+var controller = (function(budgetCtrl, UICtrl) {
+  document.querySelector('.add__btn').addEventListener('click', function(){
+  
+  });
+})(budgetController, UIController);
