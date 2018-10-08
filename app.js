fe8bc80a49d94c4bae26c3ef34f827e242a3433c
wallandall <wallandall@gmail.com>
2018-10-08 23:57:18 +0200

    Reading input

--- app.js
+++ app.js
@@ -6,15 +6,36 @@ budgetController = (function(){
 
 //UI Controller
 var UIController = (function(){
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
 
+  };
+
+  return{
+    getInput: function(){
+      return{
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function(){
+      return DOMstrings;
+    }
+  };
 })();
 
 //APP
 var controller = (function(budgetCtrl, UICtrl){
+  var DOM = UICtrl.getDOMstrings();
   var ctrlAddItem = function(){
-    console.log("Test");
+    var input = UICtrl.getInput();
+    console.log(input);
   }
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event){
     if(event.keyCode === 13 || event.which === 13){
