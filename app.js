7e4fda6fcd145b24d17ed57d994e25375bb3821e
wallandall <wallandall@gmail.com>
2018-10-10 12:22:12 +0200

    Init function and displaying output

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
 //Budget controller
 budgetController = (function(){
 
+  var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return{
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      if (data.allItems[type].length > 0) {
+       ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === 'exp'){
+        newItem = new Expense(ID, des, val);
+      } else if (type == 'inc'){
+        newItem = new Income(ID, des, val);
+      }
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+    testing: function(){
+      console.log(data);
+    }
+  };
+
 })();
 
 
 //UI Controller
 var UIController = (function(){
+
   var DOMstrings = {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputButton: '.add__btn'
-
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return{
@@ -22,6 +70,23 @@ var UIController = (function(){
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+
+    addListItem: function(obj, type){
+      var html, newHtml, element;
+      if (type === 'inc'){
+        element = DOMstrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else {
+        element = DOMstrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+    },
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -30,16 +95,33 @@ var UIController = (function(){
 
 //APP
 var controller = (function(budgetCtrl, UICtrl){
-  var DOM = UICtrl.getDOMstrings();
-  var ctrlAddItem = function(){
-    var input = UICtrl.getInput();
-    console.log(input);
-  }
-  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+  var setupEventListeners = function(){
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event){
+      if(event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+      }
+    });
+  };
 
-  document.addEventListener('keypress', function(event){
-    if(event.keyCode === 13 || event.which === 13){
-      ctrlAddItem();
+  var ctrlAddItem = function(){
+    var input, newItem;
+    input = UICtrl.getInput();
+
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    UICtrl.addListItem(newItem, input.type);
+  };
+
+  return{
+    init: function(){
+      console.log('Application has started');
+      setupEventListeners();
     }
-  });
+  }
+
 })(budgetController, UIController);
+
+
+controller.init();
